dd0836f0bf9ecba77cea006678e68d7ac3ff5ebc
winvm-deepak <deepaksy28@gmail.com>
2025-02-26 08:45:59 -0800

    Update AppConfiguration

--- src/main/java/com/ttl/configuration/AppConfiguration.java
+++ src/main/java/com/ttl/configuration/AppConfiguration.java
@@ -1,5 +1,20 @@
 package com.ttl.configuration;
 
-public class AppConfiguration {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppConfiguration {
+	private String theme;
+	private String language;
+	private boolean  autoUpdate;
+	
+	
 }
